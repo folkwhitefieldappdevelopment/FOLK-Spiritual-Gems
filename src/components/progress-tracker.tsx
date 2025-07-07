09bf2cd493fd271b9cf64c20d2b973e5ea4afc3c
Folk Whitefield APP development <folkwhitefieldappdevelopment@gmail.com>
2025-07-07 13:40:15 +0000

    remove margin

--- src/components/progress-tracker.tsx
+++ src/components/progress-tracker.tsx
@@ -181,7 +181,7 @@ export function ProgressTracker({
         <CardTitle>Progress Checklist</CardTitle>
       </CardHeader>
       <CardContent className="p-0">
-        <div className="relative h-[640px] w-full overflow-auto border">
+        <div className="relative h-[550px] w-full overflow-auto border">
             <table className="w-full caption-bottom text-sm border-collapse">
             <TableHeader className="sticky top-0 z-30 bg-muted">
                 <TableRow>
@@ -290,3 +290,4 @@ export function ProgressTracker({
     </Card>
   );
 }
+    
