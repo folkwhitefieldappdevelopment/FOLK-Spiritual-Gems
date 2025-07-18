dd0de24caf8b4dfc6bbc9f174cacf5ac30494b2c
Folk Whitefield APP development <folkwhitefieldappdevelopment@gmail.com>
2025-07-18 11:08:22 +0000

    https://studio-3unqvkjcia-uc.a.run.app/icon-512x512.png Properties {   "

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "FOLK SPIRITUAL GEMS",
   description: "A contact management center for FOLK.",
-  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
